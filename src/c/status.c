44aa9de5c1b3feda90e2a9cf0a2aa8b4f14a9a0c
GreenHex <GreenHex@gmail.com>
2016-12-18 19:42:39 +0530

    More adjustments

--- src/c/status.c
+++ src/c/status.c
@@ -19,7 +19,7 @@ static void status_text_layer_update_proc( Layer *layer, GContext *ctx ) {
   char date_str[] = "AAA, DD-MMM-YYYY";
   GRect date_window_bounds = layer_get_bounds( layer );
   date_window_bounds.origin.x += 0;
-  date_window_bounds.origin.y -= 0;
+  date_window_bounds.origin.y -= 2;
   graphics_context_set_text_color( ctx, background_colour );
   strftime( date_str, sizeof( date_str ), DATE_STRING, &tm_time );
   graphics_draw_text( ctx, date_str, fonts_get_system_font( FONT_KEY_BITHAM_30_BLACK ), date_window_bounds,
